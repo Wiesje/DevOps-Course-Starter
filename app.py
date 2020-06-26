22c52ab0501458d3f8b663be7aa6b9b902f30dbd
wiesje <wiesje.heerik@softwire.com>
2020-06-26 16:55:39 +0100

    Move items to done

--- app.py
+++ app.py
@@ -19,12 +19,17 @@ def move_item_to_done():
     item_id = request.form.get('item_id')
     print(item_id)
 
+    link = link_builder.get_move_to_done_link(item_id)
+    print(link)
+    requests.put(link)
+
     return redirect(url_for('index'))
 
 @app.route('/new', methods=['POST'])
 def add_item():
     new_item = request.form.get('new_item')
-    requests.post(link_builder.get_new_item_link(new_item))
+    link = link_builder.get_new_item_link(new_item)
+    requests.post(link)
 
     return redirect(url_for('index'))
 
--- link_builder.py
+++ link_builder.py
@@ -2,18 +2,22 @@ import secrets
 
 class LinkBuilder:
     baseUrl = "https://api.trello.com"
-    listId = "5eeb6d36261b73280b211344"
     yourKey = secrets.TRELLO_API_KEY
     yourToken = secrets.TRELLO_API_TOKEN
 
     def __init__(self):
-        self.index_link = f"{self.baseUrl}/1/lists/{self.listId}/cards?key={self.yourKey}&token={self.yourToken}"
-        self.new_item = "" 
+        self.to_do_list = "5eeb6d36261b73280b211344"
+        self.done_list = "5eec7a03ebdf3b6d3eb2158e"
+        self.index_link = f"{self.baseUrl}/1/lists/{self.to_do_list}/cards?key={self.yourKey}&token={self.yourToken}"
+        
 
     def get_index_link(self):
         return self.index_link
 
     def get_new_item_link(self, new_item):
-        self.new_item = new_item
-        self.new_item_link = f"{self.baseUrl}/1/cards?key={self.yourKey}&token={self.yourToken}&idList={self.listId}&name={self.new_item}"
-        return self.new_item_link
+        new_item_link = f"{self.baseUrl}/1/cards?key={self.yourKey}&token={self.yourToken}&idList={self.to_do_list}&name={new_item}"
+        return new_item_link
+
+    def get_move_to_done_link(self, card_id):
+        move_to_done_link = f"{self.baseUrl}/1/cards/{card_id}?key={self.yourKey}&token={self.yourToken}&idList={self.done_list}"
+        return move_to_done_link
