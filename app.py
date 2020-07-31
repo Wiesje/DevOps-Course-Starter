8b8ffc17a4b67228b9ad4010b249200c5317ab2a
wiesje <wiesje.heerik@softwire.com>
2020-07-31 11:58:08 +0100

    use view-model

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from trello import Trello
+from view_model import ViewModel
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    item_list = Trello.fetch_all_items()
-    return render_template('index.html', item_list = item_list)
+    @app.route('/')
+    def index():
+        items = Trello.fetch_all_items()
+        return render_template('index.html', view_model=ViewModel(items))
 
-@app.route('/complete_item', methods=['POST'])
-def move_item_to_done():
-    Trello.complete_item(request.form.get('item_id'))
-    return redirect(url_for('index'))
+    @app.route('/complete_item', methods=['POST'])
+    def move_item_to_done():
+        Trello.complete_item(request.form.get('item_id'))
+        return redirect(url_for('index'))
 
-@app.route('/new', methods=['POST'])
-def add_item():
-    new_item = request.form.get('new_item')
-    Trello.add_item(new_item)
-    return redirect(url_for('index'))
+    @app.route('/new', methods=['POST'])
+    def add_item():
+        new_item = request.form.get('new_item')
+        Trello.add_item(new_item)
+        return redirect(url_for('index'))
+
+    return app
 
 if __name__ == '__main__':
-    app.run()
+    create_app.run()
