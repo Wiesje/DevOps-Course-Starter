cd30609cab4d5e9175f219f71d1425ffefeaf698
wiesje van den Heerik <wiesje.heerik@softwire.com>
2020-12-02 16:18:55 +0000

    change app_config

--- app_config.py
+++ app_config.py
@@ -3,11 +3,6 @@ import os
 
 
 class Config:
-    """Base configuration variables."""
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    if not SECRET_KEY:
-        raise ValueError("No SECRET_KEY set for Flask application. Did you forget to run setup.sh?")
-
     # Trello Configuration
     TRELLO_BASE_URL = 'https://api.trello.com/1'
     TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
